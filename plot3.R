a37735a17f2bf14333e8f7f07ab9571e44bb33d8
Dean Brundige <dean.brundige@yahoo.com>
2015-02-07 14:07:32 -0800

    Updated plot 1,2,3 R scripts and 480x480 png graphs

--- plot3.R
+++ plot3.R
@@ -15,4 +15,3 @@ with(hpc, points(DateTime, Sub_metering_2, type = "l", col = "red"))
 with(hpc, points(DateTime, Sub_metering_3, type = "l", col = "blue"))
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1), col = c("black", "red", "blue"))
 dev.off()
-
